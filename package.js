a698635fabc581d1e674fec547d5e49d271296e8
aaronjudd <aaron@ongoworks.com>
2016-04-22 11:56:32 -0700

    update version to 0.5.1
    - bump npm packages
    - publish version with PR #11

--- package.js
+++ package.js
@@ -1,21 +1,21 @@
 Package.describe({
   summary: "Google Spreadsheets",
   name: "ongoworks:google-spreadsheets",
-  version: "0.4.2_4",
+  version: "0.5.1",
   git: "https://github.com/ongoworks/meteor-google-spreadsheets"
 });
 
 Npm.depends({
-  "google-spreadsheets": "0.4.2",
+  "google-spreadsheets": "0.5.1",
   "googleclientlogin": "0.2.8",
-  "edit-google-spreadsheet": "0.2.19"
+  "edit-google-spreadsheet": "0.2.21"
 });
 
 Package.on_use(function (api) {
   api.versionsFrom("METEOR@0.9.0");
   api.imply(["underscore"], ["client", "server"]);
   api.use("check");
-  api.use("mongo@1.1.2");
+  api.use("mongo@1.1.7");
   api.export('GoogleSpreadsheets');
   api.export('GoogleClientLogin');
   api.export('GASpreadsheet');
